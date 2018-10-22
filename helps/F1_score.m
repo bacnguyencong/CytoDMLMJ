a03a0aa8bc1e7e8ec9ac9bf230eb3106aee05637
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2018-10-22 09:39:17 +0200

    update F1 scrore macro

--- helps/F1_score.m
+++ helps/F1_score.m
@@ -1,14 +1,26 @@
-function ret = F1_score(y_true, y_pred)
+function ret = F1_score(y_true, y_pred, measure_type)
     stats = confusionmatStats(y_true, y_pred);
     labels = stats.groupOrder;
     
-    ret = 0;
-    tot = 0;
-    for i = 1:length(labels),
-        cnt = sum(labels(i) == y_true);
-        ret = ret + cnt * stats.Fscore(i);
-        tot = tot + cnt;
-    end    
-    ret = ret / tot;
+    if (~exist('measure_type', 'var'))
+        measure_type = 'macro';
+    end
+        
+    if strcmp(measure_type, 'micro'),
+        ret = 0;
+        tot = 0;
+        for i = 1:length(labels),
+            cnt = sum(labels(i) == y_true);
+            ret = ret + cnt * stats.Fscore(i);
+            tot = tot + cnt;
+        end    
+        ret = ret / tot;
+    elseif strcmp(measure_type, 'macro'),
+        ret = mean(stats.Fscore);
+    else
+        error('Not supported');
+    end
 end
+
+
 
